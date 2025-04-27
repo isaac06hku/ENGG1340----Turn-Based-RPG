8fbb2bdc3c20d2d225beaf48aba61ad5dad99c2b
ClareLuan <u3639019@connect.hku.hk>
2025-04-28 05:16:43 +0800

    Update main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include "battle.h"
+#include "enenmy.h"
+#include "input.h"
+#include "location.h"
+#include "player.h"
+#inlcude "save_load.h"
+#inlcude "utils.h"
+
+void displayIntro();
+void displayArt(const std::string& filename);
+void displayFightTralala();
+void displayFightBoss();
+void clearBottomScreen() ;
+
+void clearBottomScreen() {
+    // Clear the bottom of the screen to showcase the text
+    for (int i = 0; i < 10; i++) {
+        std::cout << std::endl;
+    }
+}
+
+void displayArt(const std::string& filename) {
+    std::ifstream file(filename);
+    if (!file.is_open()) {
+        std::cout << "Failed to open the art file: " << filename << std::endl;
+        return;
+    }
+
+    std::string line;
+    while (getline(file, line)) {
+        std::cout << line << std::endl;
+    }
+    file.close();
+}
+
+void displayIntro(){
+  std::cout << "scrolling reels then black out" << std::endl;
+  displayArt("assets/ascii_art/location_art/forest_art.txt");
+  
+  std::cout << "See tung tung tung sahur..." << std::endl;
+  displayArt("assets/ascii_art/tung_tung_tung_sahur.txt");
+  
+}
+
+void displayFightTralala(){
+  std::cout << "Fighting Tralalelo!" << std::endl;
+  displayArt("assets/ascii_art/enemy_art/tralalelo_tralala.txt");
+}
+
+void displayFightBoss(){
+  std::cout << "Final Boss Fight: Bombardino Crocodile!" << std::endl;
+  displayArt("assets/ascii_art/enemy_art/bombardino_crocodilo.txt");
+}
+
+int main(){
+  displayIntro();
+  battleSequence();
+}
