68445261e93bf8755be7faeaefe87bbc7a5d3d3d
HHuangZZixuan <125820557+HHuangZZixuan@users.noreply.github.com>
2025-04-30 01:44:44 +0800

    Update battle.h

--- src/battle.h
+++ src/battle.h
@@ -7,21 +7,22 @@
 #include <ctime>
 
 
-struct character_statue {
+struct Player_item {
     std::string name;
     int speed;
     int attack;
-    int armor;
+    int armour;
     int health;
 };
 
 
 int roll(int number_dies);
 
+total_statue calculating_total_statue(Player_item Player_item);
 
-void print_board(character_statue player, character_statue enemy, int current_health_player, int current_health_enemy);
+void print_board(Player_item player, Player_item enemy, int current_health_player, int current_health_enemy);
 
 // 战斗函数声明
-bool battle(character_statue player, character_statue enemy);
+bool battle(Player_item player, Player_item enemy);
 
 #endif 
